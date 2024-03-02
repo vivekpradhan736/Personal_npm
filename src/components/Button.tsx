935530620372c75e907f985f211a6fe32f8e1fba
Vivek <vivekpradhan736@gmail.com>
2024-03-02 22:25:18 +0530

    initial commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,13 @@ import { ReactNode } from "react";
 
 type PropsType = {
     children: ReactNode;
-    // onClick?: () => void;
+    onClick?: () => void;
   };
 
-const Button = ({ children }: PropsType) => {
+const Button = ({ children, onClick }: PropsType) => {
   return (
     <button
-    //   onClick={onClick}
+      onClick={onClick}
       style={{
         padding: "10px 20px",
         fontSize: "1.2em",
